3b75e4efb0c626b51a9ee80e7cb3fa8fe5cf58b5
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-01-06 12:32:40 +0530

    User Signup Route + With Route Validation

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,11 +1,29 @@
+// Library Imports
+const { validationResult } = require("express-validator");
+
+// Model Imports.
 const User = require("../models/user");
 
-module.exports.signup = (request, response) => {
-    console.log(request.body)
+/**
+ * User Signup Method.
+ */
+exports.signup = (request, response) => {
+
+  // Check for request validation results setup by express-validator.
+  const error = validationResult(request);
+  if (!error.isEmpty()) {
+    // 422 Unprocessable Entity.
+    return response.status(422).json({ error: getErrors(error.errors) });
+  }
+
+  // Save user data in DB.
   const user = new User(request.body);
   user.save((error, data) => {
     if (error) {
-      console.log("Something Went Wrong.");
+      // 500 Internal Server Error.
+      return response
+        .status(500)
+        .json({ error: "Unable to save data in the DB." });
     }
     response.json({
       name: data.name,
@@ -14,3 +32,13 @@ module.exports.signup = (request, response) => {
     });
   });
 };
+
+/**
+ * Returns - an array of objects with error.msg and error.param properties in it.
+ */
+function getErrors(errors) {
+  return errors.map((item) => {
+    const { msg, param } = item;
+    return { msg, param };
+  });
+}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,36 @@
+// Libraries Imports.
 const express = require("express");
-const router = express.Router();
+const { check } = require("express-validator");
 
-const { check,validationResult } = require("express-validator");
+// Model Imports.
+const User = require("../models/user");
+
+// Controller Imports.
 const userController = require("../controller/auth");
 
+const router = express.Router();
+
+/**
+ * Signup Route.
+ * Name,Email And Password Validation.
+ */
 router.post(
   "/signup",
   [
     check("name", "Name should be at least 3 character long").isLength({
       min: 3,
     }),
-    check("email", "email is required").isEmail(),
-    check("password", "password should be at least 3 character long").isLength({
+    check("email", "Invalid Email Id")
+      .isEmail()
+      .bail()
+      .custom((value) => {
+        return User.find({ email: value }).then((user) => {
+          if (user.length > 0) {
+            return Promise.reject("Email Is Already Present");
+          }
+        });
+      }),
+    check("password", "Password should be at least 3 character long").isLength({
       min: 3,
     }),
   ],
