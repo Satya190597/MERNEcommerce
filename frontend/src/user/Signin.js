885fa1baf41fca9a6a45c372708835a73f55c17f
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-01-16 21:39:24 +0530

    Basic SingIn Functionaltiy Added In Front-End

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -11,11 +11,84 @@ const SignIn = () => {
     error: "",
     loading: false,
     didRedirect: false,
+    apiError: undefined,
+    apiErrors: undefined,
+    success: false,
   });
 
-  const { email, password, error, loading, didRedirect } = values;
+  const { email, password, error, loading, didRedirect, apiError, apiErrors,success } = values;
 
-  const {user} = isAuthenticated
+  const { user } = isAuthenticated();
+
+  const onChangeHandler = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  // SETUP A LOADING SCREEN.
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({
+      ...values,
+      error: false,
+    });
+    signIn({ email, password }).then((data) => {
+      if (!data.token) {
+        setValues({
+          ...values,
+          error: true,
+          apiError: data.error,
+          apiErrors: data.errors,
+        });
+        return false;
+      }
+      authenticate(data, () => {
+        setValues({
+          ...values,
+          email: "",
+          password: "",
+          loading: false,
+          success: true
+        });
+      });
+    });
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div className="alert alert-danger" style={{display:error ? "" : "none"}}>
+            {getError()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  const handleRedirect = () => {
+    if(success) {
+      if(isAuthenticated()) {
+        return <Redirect to="/" />
+      }
+    }
+  }
+
+  const getError = () => {
+    if(!apiErrors && !apiError) 
+    {
+      return;
+    }
+    if(apiError) {
+      return apiError
+    }
+    return (
+      <ul>
+        {
+          apiErrors.map((error) => <li>{error.msg}</li>)
+        }
+      </ul>
+    )
+  }
 
   const signInForm = () => {
     return (
@@ -24,13 +97,27 @@ const SignIn = () => {
           <form>
             <div className="form-group">
               <label className="text-white">Email</label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={onChangeHandler("email")}
+              />
             </div>
             <div className="form-group">
               <label className="text-white">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={onChangeHandler("password")}
+              />
             </div>
-            <button className="btn btn-success btn-block form-control">
+            <p>{JSON.stringify(values)}</p>
+            <button
+              className="btn btn-success btn-block form-control"
+              onClick={onSubmit}
+            >
               SignIn
             </button>
           </form>
@@ -41,6 +128,8 @@ const SignIn = () => {
   return (
     <Base title="SignIn Page" description="User SignIn">
       {signInForm()}
+      {errorMessage()}
+      {handleRedirect()}
     </Base>
   );
 };
