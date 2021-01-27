5d83efe3ddf5342d91f12ca801a59e2cfbc9508f
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-01-27 12:16:20 +0530

    Fix Sticky Footer Issue

--- frontend/src/core/Base.js
+++ frontend/src/core/Base.js
@@ -8,24 +8,28 @@ const Base = ({
   children = "<h1>Hello World</h1>",
 }) => {
   return (
-    <div>
-      <Menu />
-      <div className="container-fluid">
-        <div className="jumbotron bg-dark text-white text-center">
-          <h2 className="display-4">{title}</h2>
-          <p className="lead">{description}</p>
+    <div className="flex-container">
+      <div className="flex-row-top">
+        <Menu />
+        <div className="container-fluid">
+          <div className="jumbotron bg-dark text-white text-center">
+            <h2 className="display-4">{title}</h2>
+            <p className="lead">{description}</p>
+          </div>
         </div>
+        <div className={className} style={{marginBottom:'10px'}}>{children}</div>
       </div>
-      <div className={className}>{children}</div>
-      <footer className="footer bg-dark mt-auto py-3">
-        <div className="container-fluid bg-success text-white text-center">
-          <h1>If you got any questions, feel free to reach out!</h1>
-          <button className="btn btn-warning btn-log">Contact Us</button>
-        </div>
-        <div className="container">
-          <span className="text-muted">An amazing place to buy T-shirt.</span>
-        </div>
-      </footer>
+      <div className="flex-row-bottom">
+        <footer className="p-3">
+          <div className="container-fluid bg-success text-white text-center">
+            <h1>If you got any questions, feel free to reach out!</h1>
+            <button className="btn btn-warning btn-log">Contact Us</button>
+          </div>
+          <div className="container">
+            <span className="text-muted">An amazing place to buy T-shirt.</span>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
