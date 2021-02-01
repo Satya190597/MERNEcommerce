95f9fd6b8a168749fea67877feff84d28657f904
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-02-01 21:58:46 +0530

    Delete Category Functionaltiy Added

--- frontend/src/admin/ManageCategories.js
+++ frontend/src/admin/ManageCategories.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import AdminBase from "../core/AdminBase";
-import { getCategories } from "./helper/adminapicall";
+import { getCategories, deleteCategories } from "./helper/adminapicall";
+import { isAuthenticated } from "../auth/helper/index";
 
 const Categories = () => {
+  const auth = isAuthenticated();
+
   const [categoryList, setCategoryList] = useState([]);
+  const [deleteSuccessFlag, setDeleteSuccessFlag] = useState(false);
   useEffect(() => {
-    console.log("*** GETS CALLED ***")
+    console.log("*** GETS CALLED ***");
     getAllCategories();
   }, []);
 
@@ -16,16 +20,41 @@ const Categories = () => {
     });
   };
 
-  const onChangeHandler = (event,index) => {
-      const categoryListTemp = [...categoryList];
-      categoryListTemp[index] = event.target.value
-      setCategoryList(categoryListTemp)
-      console.log(categoryList)
+  const onChangeHandler = (event, index) => {
+    const categoryListTemp = [...categoryList];
+    categoryListTemp[index] = event.target.value;
+    setCategoryList(categoryListTemp);
+  };
+
+  const deleteCategoryHandler = (categoryId) => {
+    deleteCategories(auth.data._id, auth.token, categoryId)
+      .then((response) => {
+        console.log(response);
+        getAllCategories();
+        setDeleteSuccessFlag(true);
+        setTimeout(function () {
+          setDeleteSuccessFlag(false);
+        }, 3000);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const successMessageForDelete = () => {
+    return (
+      <div
+        className="alert alert-success"
+        style={{ display: deleteSuccessFlag ? "" : "none" }}
+      >
+        <h4 class="alert-heading">Category Deleted Successfully</h4>
+      </div>
+    );
   };
 
   const categoryListTemplate = () => {
-    console.log("*** GET CALLED ***")
-    console.log(categoryList)
+    console.log("*** GET CALLED ***");
+    console.log(categoryList);
     return (
       <table class="table text-white">
         <thead>
@@ -46,7 +75,7 @@ const Categories = () => {
                     type="text"
                     value={category.name}
                     className="form-control"
-                    onChange={(event) => onChangeHandler(event,index)}
+                    onChange={(event) => onChangeHandler(event, index)}
                   />
                 </td>
                 <td>
@@ -55,7 +84,10 @@ const Categories = () => {
                   </button>
                 </td>
                 <td>
-                  <button className="btn btn-danger bg-gradient rounded">
+                  <button
+                    className="btn btn-danger bg-gradient rounded"
+                    onClick={() => deleteCategoryHandler(category._id)}
+                  >
                     Delete
                   </button>
                 </td>
@@ -73,6 +105,7 @@ const Categories = () => {
       description="Edit Or Delete Your Categories"
     >
       <div className="container mt-5">
+        <div>{successMessageForDelete()}</div>
         <div>{categoryListTemplate()}</div>
       </div>
     </AdminBase>
--- frontend/src/admin/helper/adminapicall.js
+++ frontend/src/admin/helper/adminapicall.js
@@ -1,111 +1,127 @@
-import {API} from "../../backend";
+import { API } from "../../backend";
 
 // Create Category API Call.
-export const createCategory = (userId,token,category)  => {
-    return fetch(`${API}/category/create/${userId}`,{
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(category)
+export const createCategory = (userId, token, category) => {
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json();
-    })
-    .catch(error => {
-        return error.json();
-    })
-}
+    .catch((error) => {
+      return error.json();
+    });
+};
 
 export const getCategories = () => {
-    return fetch(`${API}category/all`,{
-        method: "GET"
+  return fetch(`${API}category/all`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json()
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const deleteCategories = (userId,token,categoryId) => {
+  return fetch(`${API}category/${categoryId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+    }
+  })
+    .then((response) => {
+      return response.json();
     })
-    .catch(error => {
-        console.log(error)
-    })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 //#region
 
-export const createProduct = (userId,token,product) => {
-    return fetch(`${API}/product/create/${userId}`,{
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: product
+export const createProduct = (userId, token, product) => {
+  return fetch(`${API}/product/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json()
-    })
-    .catch(error => {
-        console.log(error);
-    })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export const getAllProducts = () => {
-    return fetch(`${API}/products`,{
-        method: "GET"
+  return fetch(`${API}/products`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json()
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export const getAProduct = (productId) => {
-    return fetch(`${API}/product/${productId}`,{
-        method: "GET"
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json()
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-export const updateProduct = (productId,userId,token,product) => {
-    return fetch(`${API}/product/${productId}/${userId}`,{
-        method: "PUT",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: product
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json()
-    })
-    .catch(error => {
-        console.log(error);
-    })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-export const deleteProduct = (userId,token,productId) => {
-    return fetch(`${API}/product/${productId}/${userId}`,{
-        method: "DELETE",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        }
+export const deleteProduct = (userId, token, productId) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
     })
-    .then(response => {
-        return response.json()
-    })
-    .catch(error => {
-        console.log(error);
-    })
-}
-
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 //#endregion
