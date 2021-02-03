db9255fe3817f46c1fdb67d40a50341d8d2ecf90
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2021-02-03 22:09:23 +0530

    UI Changes In Add Category Section

--- frontend/src/admin/AddCategory.js
+++ frontend/src/admin/AddCategory.js
@@ -7,18 +7,27 @@ import { createCategory } from "./helper/adminapicall";
 const AddCategory = () => {
   const [name, setName] = useState("");
   const [error, setError] = useState(false);
+  const [validationError, setValidationError] = useState(false);
   const [success, setSuccess] = useState(false);
 
   const { data, token } = isAuthenticated();
 
+  // On change handler.
   const onChangeHandler = (event) => {
     setError(false);
+    setValidationError(false);
     setName(event.target.value);
   };
 
+  // On Submit handler - trigger create category api call.
   const onSubmit = (event) => {
     event.preventDefault();
-    setError("");
+
+    // Validate - Category Name.
+    if (name.length <= 3) {
+      setValidationError(true);
+      return;
+    }
 
     // Backend API Call Trigger.
     createCategory(data._id, token, { name }).then((data) => {
@@ -30,18 +39,28 @@ const AddCategory = () => {
       }
     });
   };
+
+  // Back navigation button.
   const goBack = () => {
     return (
       <div className="">
-        <Link className="btn btn-sm btn-warning m-3" to="/admin/dashboard">
+        <Link
+          className="btn btn-sm btn-warning m-3 bg-gradient rounded"
+          to="/admin/dashboard"
+        >
           Go Back
         </Link>
       </div>
     );
   };
+
+  // --- Validation and success messages ---
+  //#region
   const successMessage = () => {
     if (success) {
-      return <h4 className="text-success m-3">Category Created Successfully.</h4>;
+      return (
+        <h4 className="text-success m-3">Category Created Successfully.</h4>
+      );
     }
   };
   const errorMessage = () => {
@@ -49,19 +68,32 @@ const AddCategory = () => {
       return <h4 className="text-danger m-3">Failed To Create Category</h4>;
     }
   };
+  const validationMessage = () => {
+    if (validationError) {
+      return (
+        <h4 className="text-danger m-3">
+          Category name must be 3 characters long.
+        </h4>
+      );
+    }
+  };
+  //#endregion
+
   const createCategoryForm = () => {
     return (
       <form>
         <div className="form-group m-3">
-          <p className="lead">Enter the Category</p>
+          <p className="lead text-white">Enter category name.</p>
           <input
             className="form-control my-3"
-            required            
             onChange={onChangeHandler}
             value={name}
             placeholder="Example - Summer Collection"
           />
-          <button onClick={onSubmit} className="btn btn-outline-info">
+          <button
+            onClick={onSubmit}
+            className="btn btn-outline-success rounded"
+          >
             Create Category
           </button>
         </div>
@@ -77,9 +109,11 @@ const AddCategory = () => {
     >
       <div className="row">
         <div className="col-md-8 offset-md-2">
+          {goBack()}
           {successMessage()}
           {errorMessage()}
-          {createCategoryForm()} {goBack()}
+          {validationMessage()}
+          {createCategoryForm()}
         </div>
       </div>
     </AdminBase>
